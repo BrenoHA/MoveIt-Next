4889d91ee36416913430587ef32ae5a2c85d55c7
BrenoHA <breno.ha@gmail.com>
2021-02-25 11:14:52 -0300

    Adding ChallengeBox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import Head from 'next/head';
 
@@ -21,6 +22,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
+          <ChallengeBox />
         </div>
       </section>
     </div>
